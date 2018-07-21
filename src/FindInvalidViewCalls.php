62949bfa8a0226ef424d4decc64c302ed309bdf3
Karl Roberts <karl.roberts@netsells.co.uk>
2018-07-21 22:56:16 +0100

    branch: feature/add-views-command - Add view command

--- src/FindInvalidViewCalls.php
+++ src/FindInvalidViewCalls.php
@@ -4,17 +4,18 @@ namespace Juddling\RouteChecker;
 
 use Illuminate\Support\Collection;
 use Illuminate\Routing\Route;
+use Illuminate\View\FileViewFinder;
+use InvalidArgumentException;
 use PhpParser\Node;
 use PhpParser\NodeTraverser;
 use PhpParser\ParserFactory;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class FindInvalidRouteCalls extends FindInvalid
+class FindInvalidViewCalls extends FindInvalid
 {
 	/** @var Collection */
 	protected $viewPaths;
 	protected $nameOfArgument = 'View Path';
-
 
 	protected function getFunctionName()
 	{
@@ -23,13 +24,15 @@ class FindInvalidRouteCalls extends FindInvalid
 
 	protected function check(string $viewPath): bool
 	{
-		if ($this->viewPaths) {
-			return $this->viewPaths->contains($viewPath);
+		/** @var FileViewFinder $views */
+		$views = app('view.finder');
+
+		try {
+			$views->find($viewPath);
+		} catch (InvalidArgumentException $e) {
+			return false;
 		}
 
-		// Check if the view is present
-
-		// now try this function again
-		return $this->check($viewPath);
+		return true;
 	}
 }
