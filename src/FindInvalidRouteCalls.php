739ddf497f724547c604241f1753349e3e756eaa
James Judd <james.judd@netsells.co.uk>
2018-07-21 22:19:24 +0100

    switch to spaces

--- src/FindInvalidRouteCalls.php
+++ src/FindInvalidRouteCalls.php
@@ -4,35 +4,31 @@ namespace Juddling\RouteChecker;
 
 use Illuminate\Support\Collection;
 use Illuminate\Routing\Route;
-use PhpParser\Node;
-use PhpParser\NodeTraverser;
-use PhpParser\ParserFactory;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class FindInvalidRouteCalls extends FindInvalid
 {
-	/** @var Collection */
-	protected $routeNames;
-	protected $nameOfArgument = 'Route Names';
+    /** @var Collection */
+    protected $routeNames;
+    protected $nameOfArgument = 'Route Names';
 
 
-	protected function getFunctionName()
-	{
-		return 'route';
-	}
+    protected function getFunctionName()
+    {
+        return 'route';
+    }
 
-	protected function check(string $routeName): bool
-	{
-		if ($this->routeNames) {
-			return $this->routeNames->contains($routeName);
-		}
+    protected function check(string $routeName): bool
+    {
+        if ($this->routeNames) {
+            return $this->routeNames->contains($routeName);
+        }
 
-		// set the routes
-		$this->routeNames = collect(\Route::getRoutes())->map(function (Route $route) {
-			return $route->getName();
-		})->filter();
+        // set the routes
+        $this->routeNames = collect(\Route::getRoutes())->map(function (Route $route) {
+            return $route->getName();
+        })->filter();
 
-		// now try this function again
-		return $this->check($routeName);
-	}
+        // now try this function again
+        return $this->check($routeName);
+    }
 }
