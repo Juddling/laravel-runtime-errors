255261006b326650e54ac6ece14c566282cd3613
James Judd <james.judd@netsells.co.uk>
2018-08-07 22:22:24 +0100

    replace tabs

--- src/FindInvalidViewCalls.php
+++ src/FindInvalidViewCalls.php
@@ -8,32 +8,32 @@ use InvalidArgumentException;
 
 class FindInvalidViewCalls extends FindInvalid
 {
-	/** @var Collection */
-	protected $viewPaths;
-	/** @var string $nameOfArgument */
-	protected $nameOfArgument = 'View Path';
-	/** @var FileViewFinder $views */
-	protected $viewFinder;
+    /** @var Collection */
+    protected $viewPaths;
+    /** @var string $nameOfArgument */
+    protected $nameOfArgument = 'View Path';
+    /** @var FileViewFinder $views */
+    protected $viewFinder;
 
-	public function __construct()
-	{
-		$this->viewFinder = app('view.finder');
-		parent::__construct();
-	}
+    public function __construct()
+    {
+        $this->viewFinder = app('view.finder');
+        parent::__construct();
+    }
 
-	protected function getFunctionName()
-	{
-		return 'view';
-	}
+    protected function getFunctionName()
+    {
+        return 'view';
+    }
 
-	protected function check(string $viewPath): bool
-	{
-		try {
-			$this->viewFinder->find($viewPath);
-		} catch (InvalidArgumentException $e) {
-			return false;
-		}
+    protected function check(string $viewPath): bool
+    {
+        try {
+            $this->viewFinder->find($viewPath);
+        } catch (InvalidArgumentException $e) {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 }
